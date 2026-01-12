8d90ab48711356a8eb420beffe5f1cb0d9d8b91b
LNA2302 <helene.ader@laposte.net>
2026-01-12 17:33:33 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "google" {
+  project     = "infoline-devops-hiver2025"
+  region      = "us-central1"
+  credentials = file("key.json")
+}
