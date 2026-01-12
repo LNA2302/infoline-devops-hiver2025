57f089100d3d584b91e9e11e06a77fc037983f51
LNA2302 <helene.ader@laposte.net>
2026-01-12 17:32:07 +0100

    Update cloudsql.tf

--- terraform/cloudsql.tf
+++ terraform/cloudsql.tf
@@ -1,1 +1,30 @@
+resource "google_sql_database_instance" "postgres_infoline" {
+  name             = "postgres-infoline"
+  database_version = "POSTGRES_15"
+  region           = "us-central1"
 
+  settings {
+    tier = "db-f1-micro"   # plus petite instance possible
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+
+    backup_configuration {
+      enabled = false
+    }
+  }
+
+  deletion_protection = false
+}
+
+resource "google_sql_database" "infoline_db" {
+  name     = "infoline_db"
+  instance = google_sql_database_instance.postgres_infoline.name
+}
+
+resource "google_sql_user" "infoline_user" {
+  name     = "infoline_user"
+  instance = google_sql_database_instance.postgres_infoline.name
+  password = "ChangeMe123!"
+}
