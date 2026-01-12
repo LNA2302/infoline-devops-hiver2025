1bea52c781b08505b95b41f8bf5affee802200af
LNA2302 <helene.ader@laposte.net>
2026-01-12 17:34:10 +0100

    Update storage.tf

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,1 +1,15 @@
-
+resource "google_storage_bucket" "frontend_bucket" {
+  name          = "frontend-infoline-angular"
+  location      = "US"
+  force_destroy = true
+  uniform_bucket_level_access = true
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "index.html"
+  }
+}
+resource "google_storage_bucket_iam_member" "public_access" {
+  bucket = google_storage_bucket.frontend_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
