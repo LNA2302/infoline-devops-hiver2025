ab375b1ad366dceb0d5f1c7571c821a66d135af9
LNA2302 <helene.ader@laposte.net>
2026-01-12 17:32:53 +0100

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,1 +1,27 @@
+resource "google_container_cluster" "gke_infoline" {
+  name     = "gke-infoline"
+  location = "us-central1"
 
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc_infoline.name
+  subnetwork = google_compute_subnetwork.subnet_infoline.name
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.gke_infoline.name
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-small"
+    disk_size_gb = 30
+    disk_type    = "pd-standard"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
